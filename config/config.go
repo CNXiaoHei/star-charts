cb21df65810f96dc449505ea722ae04bafcfd12d
cnxiaohei <441900894@qq.com>
2022-12-26 23:47:58 +0800

    commit

--- config/config.go
+++ config/config.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+	"github.com/apex/log"
+	"github.com/caarlos0/env"
+)
+
+type Config struct {
+	RedisURL              string   `env:"REDIS_URL" envDefault:"redis://localhost:6379"`
+	GithubTokens          []string `env:"GITHUB_TOKENS"`
+	GithubPageSize        int      `env:"GITHUB_PAGE_SIZE" envDefault:"100"`
+	GithubMaxRateUsagePct int      `env:"GITHUB_MAX_RATE_LIMIT_USAGE" envDefault:"80"`
+	Listen                string   `env:"LISTEN" envDefault:"127.0.0.1:3000"`
+}
+
+func Get() (cfg Config) {
+	if err := env.Parse(&cfg); err != nil {
+		log.WithError(err).Fatal("failed to load config")
+	}
+	return
+}
--- internal/roundrobin/roundrobin.go
+++ internal/roundrobin/roundrobin.go
@@ -1,1 +1,83 @@
 package roundrobin
+
+import (
+	"fmt"
+	"github.com/apex/log"
+	"sync"
+	"sync/atomic"
+)
+
+type RoundRobiner interface {
+	Pick() (*Token, error)
+}
+
+func New(tokens []string) RoundRobiner {
+	log.Debugf("creating round robin with %d tokens", len(tokens))
+	if len(tokens) == 0 {
+		return &noTokensRoundRobin{}
+	}
+	result := make([]*Token, 0, len(tokens))
+	for _, token := range tokens {
+		result = append(result, NewToken(token))
+	}
+	return &realRoundRobin{tokens: result}
+}
+
+type noTokensRoundRobin struct{}
+
+func (n *noTokensRoundRobin) Pick() (*Token, error) {
+	return nil, nil
+}
+
+type realRoundRobin struct {
+	tokens []*Token
+	next   int64
+}
+
+func (r *realRoundRobin) Pick() (*Token, error) {
+	return r.doPick(0)
+}
+
+func (r *realRoundRobin) doPick(try int) (*Token, error) {
+	if try > len(r.tokens) {
+		return nil, fmt.Errorf("no valid tokens left")
+	}
+	idx := atomic.LoadInt64(&r.next)
+	atomic.StoreInt64(&r.next, (idx+1)%int64(len(r.tokens)))
+	if pick := r.tokens[idx]; pick.OK() {
+		log.Debugf("picked %s", pick.Key())
+		return pick, nil
+	}
+	return r.doPick(try + 1)
+}
+
+func NewToken(token string) *Token {
+	return &Token{token: token, valid: true}
+}
+
+type Token struct {
+	token string
+	valid bool
+	lock  sync.RWMutex
+}
+
+func (t *Token) String() string {
+	return t.token[len(t.token)-3:]
+}
+
+func (t *Token) Key() string {
+	return t.token
+}
+
+func (t *Token) OK() bool {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+	return t.valid
+}
+
+func (t *Token) Invalidate() {
+	log.Warnf("invalidate token '...%s.", t)
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	t.valid = false
+}
